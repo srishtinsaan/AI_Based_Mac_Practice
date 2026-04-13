f65454bdae92d58270d728178e104d831db9df3b
Srishti Singh <srish17816@gmail.com>
2026-04-14 00:12:49 +0530

    chart added

--- Q_learning_01/training/train.py
+++ Q_learning_01/training/train.py
@@ -4,13 +4,21 @@ from src.env.switch_env import SwitchEnv
 def run_training(data_path):
     env = SwitchEnv(data_path)
     agent = QAgent(states=env.encoder.total_states())
+    rewards_history = [] 
     
     for ep in range(2000):
-        state = env.reset() # This line will now work
+        state = env.reset()
+        total_ep_reward = 0
         for _ in range(10):
             action = agent.choose_action(state)
             next_s, reward, _ = env.step(action)
             agent.update(state, action, reward, next_s)
             state = next_s
+            total_ep_reward += reward
+        
         agent.decay_epsilon()
-    return agent, env.encoder
+        # This saves the total reward for each episode to be plotted later
+        rewards_history.append(total_ep_reward) 
+        
+    # CRITICAL: You must return all THREE variables here
+    return agent, env.encoder, rewards_history
